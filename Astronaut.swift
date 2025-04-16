23d698666422e8743c94f310fd8dc1c450990171
zoli0829 <zoli0829@gmail.com>
2025-04-16 13:44:49 +0100

    finished day 40

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Zoltan Vegh on 16/04/2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
