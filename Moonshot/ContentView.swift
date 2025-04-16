e021f0fc51419c7e32faf726556c65896ad2426a
zoli0829 <zoli0829@gmail.com>
2025-04-16 09:52:37 +0100

    Initial commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Zoltan Vegh on 16/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Zoltan Vegh on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
